a6dda6d6fd79252509c508b8668133976985d063
agrippa kellum <ask252@cornell.edu>
2024-08-29 16:50:50 -0400

    spawner!

--- Assets/RealGame/Enemies/EnemySpawner.cs
+++ Assets/RealGame/Enemies/EnemySpawner.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class EnemyController : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject enemyPrefab;
+    public Transform playerTransform;
+    public float spawnDistanceFromPlayer = 50f;
+    public float spawnInterval = 2f;
+
+    private void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        // Ensure we have necessary references
+        if (enemyPrefab == null || playerTransform == null)
+        {
+            Debug.LogError("EnemySpawner: Missing enemyPrefab or player reference!");
+            return;
+        }
+
+        // Start the spawning coroutine
+        StartCoroutine(SpawnEnemyRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnEnemyRoutine()
     {
-        
+        while (true)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        // Generate a random angle
+        float angle = Random.Range(0f, 360f);
+
+        // Calculate spawn position
+        Vector2 spawnDirection = Quaternion.Euler(0, 0, angle) * Vector2.right;
+        Vector2 spawnPosition = (Vector2)playerTransform.position + spawnDirection * spawnDistanceFromPlayer;
+
+        // Instantiate the enemy
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
     }
 }
